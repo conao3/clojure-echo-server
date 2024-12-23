0c622e91ee952ac004e51980f74bf39c63bd59b3
conao3 <conao3@gmail.com>
2024-12-24 00:04:49 +0900

    first commit

---
+++ src/echo_server/core.clj
@@ -0,0 +1,9 @@
+(ns echo-server.core
+  (:require
+   [clojure.tools.logging :as log])
+  (:gen-class))
+
+(defn -main
+  "The entrypoint."
+  [& args]
+  (log/info args))
---
+++ test/echo_server/core_test.clj
@@ -0,0 +1,6 @@
+(ns echo-server.core-test
+  (:require
+   [clojure.test :as t]))
+
+(t/deftest sample-test
+  (t/is (= true true)))
