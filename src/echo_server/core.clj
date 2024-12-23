8b63f8b28ac33a42e52f397e4906d5ce7804b90b
conao3 <conao3@gmail.com>
2024-12-24 00:51:50 +0900

    implement echo server

--- src/echo_server/core.clj
+++ src/echo_server/core.clj
@@ -1,7 +1,32 @@
 (ns echo-server.core
   (:require
-   [clojure.tools.logging :as log])
+   [clojure.tools.logging :as log]
+   [clojure.java.io :as io])
+  (:import
+   [java.net ServerSocket]
+   [java.io BufferedReader BufferedWriter])
   (:gen-class))
+
+(defn handle-client [client-socket]
+  (let [input-stream ^BufferedReader (io/reader client-socket)
+        output-stream ^BufferedWriter (io/writer client-socket)]
+    (loop []
+      (let [inpt (. input-stream readLine)]
+        (println "Received: " inpt)
+        (when (and inpt (not= "" inpt))
+          (. output-stream write (str inpt "\n"))
+          (. output-stream flush))
+        (when-not (nil? inpt)         ; nil indicates EOF
+          (recur))))))
+
+(defn start-server []
+  (println "Listening localhost:15154")
+  (let [server-socket (ServerSocket. 15155)]
+    (loop []
+      (let [client-socket (. server-socket accept)]
+        (println "Connection Accepted")
+        (handle-client client-socket))
+      (recur))))
 
 (defn -main
   "The entrypoint."
